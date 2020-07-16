a3d81a1b0de8a7d0f0105121ab1d4e485c343d2e
karice <kl529@naver.com>
2020-07-16 23:48:58 +0900

    first commit

--- app/src/main/java/com/example/engineering_school/MainActivity.java
+++ app/src/main/java/com/example/engineering_school/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.engineering_school;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
